e7efecca895ad70ea5c765f0ba59693a7fa80c04
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-19 16:59:14 +0530

    initialised server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const { PORT, DB_URL } = require("./config");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Connection Successful"))
+  .catch((e) => console.error(e));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Connected at Port ${PORT}`);
+});
