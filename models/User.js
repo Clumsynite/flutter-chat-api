a954f3e74af9de78b379e5c770297fb3c34a7385
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-20 12:42:59 +0530

    updated User model
    - store timestamps
    - store data for user presence

--- models/User.js
+++ models/User.js
@@ -1,48 +1,61 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: (value) => {
-        const re = /^[a-zA-Z0-9]{4,15}$/; // https://stackoverflow.com/a/6814901/13762501
-        return value.match(re);
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: (value) => {
+          const re = /^[a-zA-Z0-9]{4,15}$/; // https://stackoverflow.com/a/6814901/13762501
+          return value.match(re);
+        },
+        message: "Please enter a valid username",
       },
-      message: "Please enter a valid username",
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: (value) => {
+          const re =
+            /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+          return value.match(re);
+        },
+        message: "Please enter a valid email address",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: { validator: (value) => value.length > 6, message: "Password must be of more than 6 characters" },
+    },
+    avatar: {
+      type: String,
+    },
+    firstName: {
+      type: String,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    friends: [String],
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
+    lastSeen: {
+      type: Date,
     },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: (value) => {
-        const re =
-          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        return value.match(re);
-      },
-      message: "Please enter a valid email address",
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    validate: { validator: (value) => value.length > 6, message: "Password must be of more than 6 characters" },
-  },
-  avatar: {
-    type: String,
-  },
-  firstName: {
-    type: String,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", UserSchema);
 
