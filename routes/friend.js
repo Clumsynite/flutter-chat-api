252b60717fe839367addc7b9df90978d1cd8d5b0
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-20 21:57:45 +0530

    renamed friend-request routes

--- routes/friend.js
+++ routes/friend.js
@@ -5,11 +5,12 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/friend-request/to", auth, friendController.getFriendRequestsReceived);
-router.post("/friend-request/accept", auth, friendController.acceptFriendRequest);
-router.post("/friend-request/cancel", auth, friendController.cancelFriendRequest);
-router.get("/friend/count", auth, friendController.getFriendRequestCount);
-router.post("/friend/", auth, friendController.createfriendRequest);
-router.delete("/friend/:to", auth, friendController.deletefriendRequest);
+router.get("/friend-request/to", auth, friendController.getFriendRequestsReceived); // get a list of all received friend requests
+router.get("/friend-request/count", auth, friendController.getFriendRequestCount); // get friend request count
+router.post("/friend-request/cancel", auth, friendController.cancelFriendRequest); // cancel friend request by request id
+router.post("/friend-request/", auth, friendController.createfriendRequest); // create new friend request
+router.delete("/friend-request/:to", auth, friendController.deletefriendRequest); // delete friend request by contact id
+
+router.post("/friend/", auth, friendController.acceptFriendRequest); // accept friend request
 
 module.exports = router;
