94fa12a4571d339bdad5082e833eb3f031d9e685
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-22 19:03:30 +0530

    added socket to handle presence

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const { PORT, DB_URL } = require("./config");
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const friendRouter = require("./routes/friend");
+const { handleClientOnline, handleClientOffline } = require("./socket/presence");
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, { cors: { origin: "*" } });
+io.on("connection", (client) => {
+  client.on("client_online", (id) => handleClientOnline({ id, io }));
+  client.on("client_offline", (id) => handleClientOffline({ id, io }));
+});
+
+exports.io = io;
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -24,6 +36,12 @@ mongoose
   .then(() => console.log("Connection Successful"))
   .catch((e) => console.error(e));
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Connected at Port ${PORT}`);
+/** catch 404 and forward to error handler */
+app.use("*", (req, res) => res.status(500).json({ error: "API endpoint doesn't exists" }));
+
+/** Create HTTP server. */
+server.listen(PORT, "0.0.0.0");
+/** Event listener for HTTP server "listening" event. */
+server.on("listening", () => {
+  console.log(`Listening on port :: ${server.address().port}`);
 });
---
+++ socket/presence.js
@@ -0,0 +1,30 @@
+const User = require("../models/User");
+
+const handleClientOnline = async ({ id, io }) => {
+  const user = await User.findById(id);
+  if (user) {
+    user.isOnline = true;
+    await user.save();
+    for (let i = 0; i < user.friends.length; i += 1) {
+      const emitKey = `${user.friends[i]}_online`;
+      io.emit(emitKey, id);
+    }
+  }
+};
+
+const handleClientOffline = async ({ id, io }) => {
+  const user = await User.findById(id);
+  if (user) {
+    user.isOnline = false;
+    await user.save();
+    for (let i = 0; i < user.friends.length; i += 1) {
+      const emitKey = `${user.friends[i]}_offline`;
+      io.emit(emitKey, id);
+    }
+  }
+};
+
+module.exports = {
+  handleClientOnline,
+  handleClientOffline,
+};
