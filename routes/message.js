85426571c368d6a5d980275ba7ac9881c480afb6
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-29 16:23:46 +0530

    updated comments on message routes

--- routes/message.js
+++ routes/message.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.get("/message/from/:friend", auth, messageController.getMessagesForId); // get a list of all messages from a user
 router.post("/message/", auth, messageController.sendMessage); // send a new message and notify user of the new message
-router.delete("/message/from/:friend", auth, messageController.deleteAllMessages); // send a new message and notify user of the new message
-router.delete("/message/:ids", auth, messageController.deleteSelectedMessages); // send a new message and notify user of the new message
-
+router.delete("/message/from/:friend", auth, messageController.deleteAllMessages); // delete all messages sent to and received from a friend
+router.delete("/message/:ids", auth, messageController.deleteSelectedMessages); // delete a list of messages by id
 module.exports = router;
